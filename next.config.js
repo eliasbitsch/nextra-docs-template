5c8a37f22c1ad1f6574ce7ffcb45fa642720b8ef
Elias Bitsch <eliasbitsch@hotmail.com>
2024-09-25 23:28:52 +0200

    Update next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true,
+  output: 'export',
+    images: {
+    unoptimized: true,
+  },
 })
 
 module.exports = withNextra()
