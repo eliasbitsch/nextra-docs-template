70932cb07560e5ec257af34d3426ca68033a319d
Elias Bitsch <eliasbitsch@hotmail.com>
2024-09-29 22:29:00 +0200

    Refactor theme.config.tsx to update logo and remove unnecessary content

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,30 +1,26 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+const Logo = React.createElement('span', null, 'ROS-Ready')
+
 const config: DocsThemeConfig = {
-  logo: <span>ROS-Ready</span>,
   project: {
     // link: 'https://github.com/shuding/nextra-docs-template',
   },
+  logo: Logo,
   chat: {
     // link: 'https://twitter.com',
   },
   // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: '',
-
+    text: ' ',
   },
   feedback: {
-    content: '', // This disables the feedback button
-
+    content: ' ', // This disables the feedback button
   },
-
   editLink: {
     text: ' ', // This makes the link text a space
   },
-
 }
-
-
 
 export default config
