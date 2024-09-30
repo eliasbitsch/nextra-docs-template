56d65573785bc7608bf2b335efaaf8e6e26b64e6
Elias Bitsch <eliasbitsch@hotmail.com>
2024-09-30 11:39:13 +0200

    Refactor next.config.js to remove unnecessary code

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-// next.config.js
-
-/** @type {import('next').NextConfig} */
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true,
 });
 
-module.exports = withNextra({
-  // Any other Next.js configuration can go here
-  images: {
-    unoptimized: false,  // or false, depending on your requirements
-  },
-});
+module.exports = withNextra()
